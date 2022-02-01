8ffa7d06ec5582be5d28589805bdf1fde5d865fe
Jaswanth Reddy <Jaswanth Reddy@Jaswanthreddy>
2022-02-01 10:57:28 +0530

    Assignment

---
+++ firstp/src/firstp/firstp.java
@@ -0,0 +1,24 @@
+package firstp;
+
+import java.util.Scanner;
+
+public class firstp {
+
+	public static void main(String[] args) {
+		String str1 = "hello";
+		String str2 = "java";
+		String str3 = "hello";
+		String str4 = "java";
+		int a = str1.compareTo(str2);
+		System.out.println(str1.length() + str2.length());
+		if (a<0) {
+			System.out.println("no");
+		}
+		else {
+			System.out.println("yes");
+			}
+		
+	    
+	}
+
+}
