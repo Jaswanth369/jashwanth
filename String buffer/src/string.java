1d30b7dce28e12910aeebb80cd52409b51d85d8d
Jaswanth Reddy <Jaswanth Reddy@Jaswanthreddy>
2022-02-01 12:29:58 +0530

    Assignments1

---
+++ String buffer/src/string.java
@@ -0,0 +1,21 @@
+
+public class string {
+
+	public static void main(String[] args) {
+		String str1 = "hello";
+		String str2 = "java";
+		int a = str1.compareTo(str2);
+		System.out.println(str1.length() + str2.length());
+		if (a<0) {
+			System.out.println("no");
+		}
+		else {
+			System.out.println("yes");
+			}
+		String output = str1.substring(0, 1).toUpperCase() + str1.substring(1);
+		String output1 = str2.substring(0, 1).toUpperCase() + str2.substring(1);
+		System.out.println(output +" "+ output1);
+
+	}
+
+}
---
+++ String buffer/src/stringbuffer.java
@@ -0,0 +1,18 @@
+
+
+public class stringbuffer {
+
+	public static void main(String[] args) {
+		 String s = " I am Sample";             // SAMPLE INPUT
+		 String s1 = "";
+		 for (int i = 0; i < s.length(); i++) {
+			if(Character.isUpperCase(s.charAt(i))) 
+				 s1=s1+Character.toLowerCase(s.charAt(i));
+			else 
+				 s1=s1+Character.toUpperCase(s.charAt(i));
+		}
+		System.out.println(s1);
+
+	}
+
+}
